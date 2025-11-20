824994332cb3bff5b0b2f273b76004ab9d6d7f79
BLMichaels <155132733+BLMichaels@users.noreply.github.com>
2025-11-20 14:43:29 -0500

    SimBox Abdominal Trauma
    SimBox Abdominal Trauma

---
+++ story_content/triggers.js
@@ -0,0 +1,41 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wnpnp5QETq":
+        Script1();
+        break;
+      case "6mrHrKPSoVK":
+        Script2();
+        break;
+      case "6WVnrFngbe9":
+        Script3();
+        break;
+      case "5nSS7KUaaHX":
+        Script4();
+        break;
+      case "5ffv0qoJxci":
+        Script5();
+        break;
+      case "5x2sFd0Dgh9":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
